154f12d137441460a06f062b4950537230f6c171
arkmadj <ahmadjinadu@gmail.com>
2025-07-10 06:42:02 +0100

    fix build errors

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,10 +1,15 @@
-import { FaHeart } from 'react-icons/fa';
-import { Button } from '@/components/ui/button';
-function FavoriteToggleButton() {
+import { FaHeart } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+
+type FavoriteToggleButtonProps = {
+  productId: string;
+};
+
+function FavoriteToggleButton({ productId }: FavoriteToggleButtonProps) {
+  console.log(productId);
   return (
     <Button size='icon' variant='outline' className='p-2 cursor-pointer'>
       <FaHeart />
-
     </Button>
   );
 }
